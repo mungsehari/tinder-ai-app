9447688829e7b5eb1b8dd2adae02784a2eeb136e
mungsehari <mungse.hari@gmail.com>
2024-07-22 16:17:30 +0530

    Tinder Ai Applaction of spring boot

---
+++ src/main/java/com/hari/TinderAiBackendApplication.java
@@ -0,0 +1,14 @@
+package com.hari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiBackendApplication.class, args);
+//		sk-proj-aeZ3rwvpZqGYR8VEbRj2T3BlbkFJJDwQ6G1OMd1IkrYPmTZO
+	}
+
+}
